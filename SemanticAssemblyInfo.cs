0374c8713113ed5b62bfa35ac13cd6a4a15e5967
Bryan Apellanes <bryan@threeheadz.com>
2020-05-17 09:29:58 -0700

    initial files

---
+++ SemanticAssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using Bam.Net;
+
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.3914039107")]
+[assembly: AssemblyCommit("616459c")]
+[assembly: AssemblySemanticVersion("1.0.5-test+616459c")]
